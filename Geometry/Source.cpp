ad4a895fa3384b05cac609663f47292b63b6d3b0
Игорь <@DESKTOP-B894K3L>
2022-06-26 22:36:42 +0300

    Circle

--- Geometry/Source.cpp
+++ Geometry/Source.cpp
@@ -1,7 +1,9 @@
 ﻿//AbstractGeometry
+#define _USE_MATH_DEFINES
 #pragma warning(disable:4326)
 #include<iostream>
 #include<Windows.h>
+#include<math.h>
 using namespace std;
 
 namespace Geometry
@@ -13,6 +15,7 @@ namespace Geometry
 		red = 0x000000FF,
 		green = 0x0000FF00,
 		blue = 0x00FF0000,
+		yellow = 0x0000FFFF,
 
 		console_default = 0x07,
 		console_blue = 0x99,
@@ -21,15 +24,62 @@ namespace Geometry
 		console_yellow = 0xEE,
 		console_white = 0xFF
 	};
-
+	enum Defaults
+	{
+		min_start_x = 100,
+		max_start_x = 800,
+		min_start_y = 50,
+		max_start_y = 500,
+		min_line_width = 5,
+		max_line_width = 20,
+	};
 	class Shape
 	{
 	protected:
+		int start_x;
+		int start_y;
+		unsigned int line_width;
 		Color color;
 	public:
 		Color get_color()const
 		{
 			return color;
+		}
+		int get_start_x()const
+		{
+			return start_x;
+		}
+		int get_start_y()const
+		{
+			return start_y;
+		}
+		unsigned int get_line_width()const
+		{
+			return line_width;
+		}
+		void set_start_x(int start_x)
+		{
+			if (start_x < Defaults::min_start_x)this->start_x = Defaults::min_start_x;
+			else if (start_x > Defaults::max_start_x)this->start_x = Defaults::max_start_x;
+			else this->start_x = start_x;
+		}
+		void set_start_y(unsigned int start_y)
+		{
+			if (start_y < Defaults::min_start_y)this->start_y = Defaults::min_start_y;
+			else if (start_y > Defaults::max_start_y)this->start_y = Defaults::max_start_y;
+			else this->start_y = start_y;
+		}
+		void set_line_width(unsigned int line_width)
+		{
+			if (line_width < Defaults::min_line_width)this->line_width = Defaults::min_line_width;
+			else if (line_width > Defaults::max_line_width)this->line_width = Defaults::max_line_width;
+			else this->line_width = line_width;
+		}
+		Shape(int start_x, int start_y, unsigned int line_width, Color color) :color(color)
+		{
+			set_start_x(start_x);
+			set_start_y(start_y);
+			set_line_width(line_width);
 		}
 
 		Shape(Color color) :color(color) {}
@@ -162,6 +212,59 @@ namespace Geometry
 			Shape::info();
 		}
 	};
+
+	class Circle :public Shape
+	{
+		double radius;
+	public:
+		double get_radius()const
+		{
+			return radius;
+		}
+		void set_radius(double radius)
+		{
+			if (radius >= 10 && radius >= 500) this->radius = radius;
+			else if (radius < 10)this->radius = 10;
+			else this->radius = 500;
+		}
+		Circle(double radius, int start_x, int start_y, unsigned int line_width, Color color) 
+			:Shape(start_x, start_y, line_width, color)
+		{
+			set_radius(radius);
+		}
+
+		~Circle(){}
+		double get_area()const
+		{
+			return M_PI * radius*radius;
+		}
+		double get_perimeter()const
+		{
+			return 2 * M_PI*radius;
+		}
+		void draw()const
+		{
+			HWND hwnd = GetConsoleWindow();
+			HDC hdc = GetDC(hwnd);
+			HPEN hPen = CreatePen(PS_SOLID, line_width, color);
+			HBRUSH hBrush = CreateSolidBrush(color);
+
+			SelectObject(hdc, hPen);
+			SelectObject(hdc, hBrush);
+
+			::Ellipse(hdc, start_x, start_y, start_x + radius * 2, start_y + radius * 2);
+
+			DeleteObject(hBrush);
+			DeleteObject(hPen);
+			ReleaseDC(hwnd, hdc);
+		}
+		void info()const
+		{
+			cout << typeid(*this).name() << endl;
+			cout << "Радиус круга:" << get_radius() << endl;
+			Shape::info();
+		}
+	};
 }
 
 void main()
@@ -176,4 +279,8 @@ void main()
 	square.info();
 	Geometry::Rectangle rect(50, 30, Geometry::Color::blue);
 	rect.info();
+
+	Geometry::Circle circle(50, 10, 50, 15, Geometry::Color::yellow);
+	circle.info();
+	
 }
